2eabe1d4c9a1f332efbddf8396cb9fad7f9fb155
kwYoohae <gocksdl7979@gmail.com>
2022-05-14 22:45:37 +0900

    first commmit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() throws InterruptedException {
+        for(int i = 0; i < 15; i++){
+            Thread.sleep(1000);
+            System.out.println((i+1) + "초입니다.");
+        }
+        return "index.html";
+    }
+}
